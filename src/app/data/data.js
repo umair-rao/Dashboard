9d5936d0135f3bea759dad5817baac2f70bfb52d
Umair Rao <umairmuqeem@gmail.com>
2023-10-11 16:19:51 +0500

    Add example data

---
+++ src/app/data/data.js
@@ -0,0 +1,113 @@
+export const data = [
+    {
+      id: 1,
+      name: {
+        first: 'Umair',
+        last: 'Muqeem',
+      },
+      total: 2795.95,
+      status: 'On Hold',
+      method: 'PayPal',
+      date: '15 Minutes ago',
+    },
+    {
+      id: 2,
+      name: {
+        first: 'Osman',
+        last: 'Ahmed',
+      },
+      total: 1195.95,
+      status: 'Processing',
+      method: 'PayPal',
+      date: '23 Minutes ago',
+    },
+    {
+      id: 3,
+      name: {
+        first: 'Maheerah',
+        last: 'Khalid',
+      },
+      total: 495.85,
+      status: 'Completed',
+      method: 'Visa',
+      date: '1 Hour ago',
+    },
+    {
+      id: 4,
+      name: {
+        first: 'Kainat',
+        last: 'Zinab',
+      },
+      total: 150.45,
+      status: 'Processing',
+      method: 'MasterCard',
+      date: '1 Hour ago',
+    },
+    {
+      id: 5,
+      name: {
+        first: 'Sarah',
+        last: 'Ali',
+      },
+      total: 175.25,
+      status: 'On Hold',
+      method: 'PayPal',
+      date: '2 Hour ago',
+    },
+    {
+      id: 6,
+      name: {
+        first: 'Zahid',
+        last: 'Baig',
+      },
+      total: 1295.75,
+      status: 'Completed',
+      method: 'Check',
+      date: '3 Hour ago',
+    },
+    {
+      id: 7,
+      name: {
+        first: 'Saad',
+        last: 'Anwar',
+      },
+      total: 89.95,
+      status: 'Completed',
+      method: 'MasterCard',
+      date: '3 Hour ago',
+    },
+    {
+      id: 8,
+      name: {
+        first: 'Mohammad Rizwan',
+        last: 'Bond',
+      },
+      total: 689.45,
+      status: 'Completed',
+      method: 'Visa',
+      date: '7 Hour ago',
+    },
+    {
+      id: 9,
+      name: {
+        first: 'Babar',
+        last: 'Azam',
+      },
+      total: 14.99,
+      status: 'Completed',
+      method: 'PayPal',
+      date: '1 Day ago',
+    },
+    {
+      id: 10,
+      name: {
+        first: 'Abdullah',
+        last: 'Shafique',
+      },
+      total: 218.99,
+      status: 'Completed',
+      method: 'MasterCard',
+      date: '1 Day ago',
+    },
+  ];
+  
