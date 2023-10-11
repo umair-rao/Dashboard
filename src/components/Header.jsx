e48ef1152bba1aeca28f6daaf1be6e83c22a1a05
Umair Rao <umairmuqeem@gmail.com>
2023-10-11 12:44:18 +0500

    Complete Sidebar and Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+
 
 const Header = () => {
-  return (
-    <div className='flex justify-between px-4 pt-4'>
-      <h2>Dashbaord</h2>
-      <h2>Welcome Back, Umair</h2>
-
-    </div>
-  )
-}
-
-export default Header
+    return (
+      <div className='flex justify-between px-4 pt-4 pl-28'>
+          <h2>Dashboard</h2>
+          <h2>Welcome Back, Clint</h2>
+      </div>
+    )
+  }
+  
+  export default Header
