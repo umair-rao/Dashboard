f8ecc75de216c43752ca809c9d32b7d896e639c1
Umair Rao <umairmuqeem@gmail.com>
2023-10-11 13:12:01 +0500

    Complete TopCrds

--- src/components/TopCards.jsx
+++ src/components/TopCards.jsx
@@ -1,10 +1,35 @@
-
 const TopCards = () => {
   return (
-    <div className="bg-slate-200">
-        <h1>Top Cards</h1>
+    <div className="grid lg:grid-cols-5 gap-4 p-4 pl-24">
+      <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">$9,484</p>
+          <p className="text-gray-600">Daily Revenue</p>
+        </div>
+        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+          <span className="text-green-700 text-lg">+18%</span>
+        </p>
+      </div>
+      <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">$2,523,994</p>
+          <p className="text-gray-600">YTD Revenue</p>
+        </div>
+        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+          <span className="text-green-700 text-lg">+21%</span>
+        </p>
+      </div>
+      <div className="bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">25,545</p>
+          <p className="text-gray-600">Customers</p>
+        </div>
+        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+          <span className="text-green-700 text-lg">+21%</span>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopCards
+export default TopCards;
